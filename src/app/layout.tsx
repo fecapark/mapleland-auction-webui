3beee2b4fad4d9f7c8238a45e4138c8ab3ce0533
fecapark <fecapark@gmail.com>
2024-02-03 01:17:34 +0900

    feat: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,6 +75,24 @@ export default function RootLayout({
           name="google-adsense-account"
           content={`ca-pub-${process.env.NEXT_PUBLIC_adsKey}`}
         ></meta>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </head>
       <body className={`${Pretendard.className} ${NotoSansKR.className}`}>
         <TanstackProvider>
